230e6c0405b6490acade6682b7d98d410b61469a
Guy Freeman <git@gfrm.in>
2017-04-13 18:06:07 +0800

    Get licence types dynamically

--- app.R
+++ app.R
@@ -15,16 +15,12 @@ library(DT)
 
 hksfclicences <- tbl(src_postgres("hkdata"), "hksfclicences", n = Inf) %>% collect %>% unique
 hksfclicences$licence <- "✔" 
+
+licencetypes <- sort(unique(hksfclicences$acttype)) # get licence types dynamically
+
 hksfclicences <- hksfclicences %>% spread(acttype, licence, fill = "")
 
-licencetypes <- c("Type 1: dealing in securities",
-  "Type 2: dealing in futures contracts",
-  "Type 4: advising on securities",
-  "Type 5: advising on futures contracts",
-  "Type 6: advising on corporate finance",
-  "Type 7: providing automated trading services",
-  "Type 9: asset management",
-  "Type 10: providing credit rating services")
+
 
 ui <- fluidPage(
    
